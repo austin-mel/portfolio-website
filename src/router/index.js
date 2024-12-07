369084577db03b095c3e0d5cad67fcdce39318b5
austin-mel <austin.csus@gmail.com>
2024-12-07 14:56:09 -0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Projects from '@/views/Projects.vue'
+import Resume from '@/views/Resume.vue'
+import Home from '@/views/Home.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/projects',
+      name: 'Projects',
+      component: Projects,
+    },
+    {
+      path: '/resume',
+      name: 'Resume',
+      component: Resume,
+    },
+  ],
+})
+
+export default router
