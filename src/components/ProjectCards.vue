56281fbffe96b564cffac8802dce8a1bddac6ff6
austin-mel <austin.csus@gmail.com>
2024-12-08 23:15:00 -0800

    Added project section tabs

--- src/components/ProjectCards.vue
+++ src/components/ProjectCards.vue
@@ -1,6 +1,12 @@
+<script setup>
+import { ref } from "vue";
+</script>
+
 <template>
-    <div>
-      <h1>Home</h1>
+    <div class="h-96 bg-light-beige">
+      <h1 v-show="DA">test1</h1>
+      <h1 v-show="project">test2</h1>
+      <h1 v-show="project">test3</h1>
     </div>
 </template>
   
--- src/components/ProjectTabs.vue
+++ src/components/ProjectTabs.vue
@@ -1,9 +1,42 @@
 <script setup>
+import ProjectCards from './ProjectCards.vue'
+import { ref } from "vue";
+
+const Project = ref('DA');
 </script>
 
 <template>
-  <div>
-    <h2>test</h2>
+  <div class="flex justify-center w-full h-16">
+    <button v-if="Project !== 'DA'" @click="Project = 'DA'" class="bg-dark-beige hover:bg-light-beige text-black font-bold py-2 px-4 border-b-4 border-theme-blue w-3/12 h-full">
+    Data Analysis
+  </button>
+  <button v-if="Project === 'DA'" @click="Project = 'DA'" class="bg-theme-blue hover:bg-light-beige text-dark-beige font-bold py-2 px-4 border-b-4 border-theme-blue w-3/12 h-full">
+    Data Analysis
+  </button>
+  <button v-if="Project !== 'ML'" @click="Project = 'ML'" class="bg-dark-beige hover:bg-light-beige text-black font-bold py-2 px-4 border-b-4 border-theme-blue w-3/12 h-full">
+    Machine Learning
+  </button>
+  <button v-if="Project === 'ML'" @click="Project = 'ML'" class="bg-theme-blue hover:bg-light-beige text-dark-beige font-bold py-2 px-4 border-b-4 border-theme-blue w-3/12 h-full">
+    Machine Learning
+  </button>
+  <button v-if="Project !== 'SE'" @click="Project = 'SE'" class="bg-dark-beige hover:bg-light-beige text-black font-bold py-2 px-4 border-b-4 border-theme-blue w-3/12 h-full">
+    Software Engineering
+  </button>
+  <button v-if="Project === 'SE'" @click="Project = 'SE'" class="bg-theme-blue hover:bg-light-beige text-dark-beige font-bold py-2 px-4 border-b-4 border-theme-blue w-3/12 h-full">
+    Software Engieering
+  </button>
   </div>
+
+  <div class="w-full h-96 bg-light-beige">
+      <div v-if="Project === 'DA'">
+        
+      </div>
+      <div v-else-if="Project === 'ML'">
+
+      </div>
+      <div v-else-if="Project === 'SE'">
+
+      </div>
+    </div>
 </template>
   
