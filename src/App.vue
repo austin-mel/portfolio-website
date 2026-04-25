b22e0a87a38c22108f57a396f3c20b6b70da10a1
austin-mel <austin.mel@mail.com>
2026-04-25 15:47:23 -0700

    Update header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import NavBar from '@/components/Navigation/Header/NavBar.vue';
+import { NavBar, Footer } from '@/components';
 </script>
 
 <template>
   <NavBar />
   <router-view />
   <!-- <ProjectModal /> -->
+  <Footer />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,5 @@
 <script setup lang="ts">
-import HeroSection from '@/components/Home/HeroSection.vue';
-import MarqueeStrip from '@/components/MarqueeStrip/MarqueeStrip.vue';
-import AboutSection from '@/components/Home/AboutSection.vue';
-import NumbersSection from '@/components/Home/NumbersSection.vue';
-import SkillsSection from '@/components/Home/SkillsSection.vue';
-import FeaturedWork from '@/components/Home/FeaturedWork.vue';
-import TimelineSection from '@/components/Home/TimelineSection.vue';
-import CtaSection from '@/components/Home/CtaSection.vue';
+
 import { useScrollObserver } from '@/composables/useScrollObserver';
  
 useScrollObserver();
@@ -14,13 +7,6 @@ useScrollObserver();
  
 <template>
   <div class="page">
-    <HeroSection />
-    <MarqueeStrip />
-    <AboutSection />
-    <NumbersSection />
-    <SkillsSection />
-    <FeaturedWork />
-    <TimelineSection />
-    <CtaSection />
+
   </div>
 </template>
